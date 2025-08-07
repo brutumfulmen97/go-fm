cb55d5091203e19248a776d189fe05e0388e4db7
vlatko <vlatkooop@gmail.com>
2025-08-07 21:58:07 +0200

    updating workouts

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,6 +9,7 @@ func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
 	r.Get("/health", app.HealthCheck)
 	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
+	r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkoutByID)
 	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
 
 	return r
